3c81abe95fb63c0176603fc83797b52079aeb44b
Renato Bispo <renatosilvabispo@outlook.com>
2022-04-21 15:46:15 -0300

    Freeze docker configuration at this point before simplifying it

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import helmet from 'helmet';
+import prisma from '../prisma';
 
 const app = express();
 
@@ -8,5 +9,10 @@ app.use(helmet());
 
 // TODO: Routes
 app.get('/ping', (_req, res) => res.status(200).json({ message: 'pong' }));
+app.get('/types', async (_req, res) => {
+  const types = await prisma.type.findMany({ select: { name: true } });
+
+  res.status(200).json(types.map(({ name }) => name));
+});
 
 export default app;
