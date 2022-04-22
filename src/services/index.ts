f170bc636742362c47a15fe450b0f97d9b7e8cfc
Renato Bispo <renatosilvabispo@outlook.com>
2022-04-22 16:57:44 -0300

    Create status service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,7 @@
 import AuthService from './auth';
 import CryptoService from './crypto';
 import LoginService from './login';
+import StatusService from './status';
 import TypeService from './type';
 
-export { AuthService, CryptoService, LoginService, TypeService };
+export { AuthService, CryptoService, LoginService, StatusService, TypeService };
