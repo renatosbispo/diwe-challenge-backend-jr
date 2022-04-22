8dff996969e6acac1147d92ab0e7967a9ccd0042
Renato Bispo <renatosilvabispo@outlook.com>
2022-04-22 19:38:21 -0300

    Write tests for invalid information on request body

--- tests/post-financial-entries.test.ts
+++ tests/post-financial-entries.test.ts
@@ -17,6 +17,20 @@ describe('POST /financial-entries', () => {
   });
 
   describe('If token is valid', () => {
+    let token: string;
+
+    beforeEach(async () => {
+      await statusSeeder.seed();
+      await typeSeeder.seed();
+      await userSeeder.seed();
+
+      const response = await supertest(app)
+        .post('/login')
+        .send({ email: 'tony@soprano.com', password: 'theboss' });
+
+      token = response.body.token;
+    });
+
     describe('If the request body contains amount, description, statusId and typeId', () => {
       const financialEntryData = {
         amount: 3000000,
@@ -28,16 +42,6 @@ describe('POST /financial-entries', () => {
       let response: Response;
 
       beforeEach(async () => {
-        await statusSeeder.seed();
-        await typeSeeder.seed();
-        await userSeeder.seed();
-
-        const {
-          body: { token },
-        } = await supertest(app)
-          .post('/login')
-          .send({ email: 'tony@soprano.com', password: 'theboss' });
-
         response = await supertest(app)
           .post('/financial-entries')
           .set('Authorization', token)
@@ -63,6 +67,94 @@ describe('POST /financial-entries', () => {
 
       it('The response body should contain the financial entry data sent in the request', async () => {
         expect(response.body).toMatchObject(financialEntryData);
+      });
+    });
+
+    describe('If the request body contains invalid information', () => {
+      it('Should return an error and status code 422', async () => {
+        const responses = await Promise.all([
+          // Invalid amount type
+          supertest(app)
+            .post('/financial-entries')
+            .set('Authorization', token)
+            .send({
+              amount: 'string',
+              description: 'Proper description',
+              statusId: 1,
+              typeId: 2,
+            })
+            .expect(422),
+          // Invalid amount format
+          supertest(app)
+            .post('/financial-entries')
+            .set('Authorization', token)
+            .send({
+              amount: 10.59,
+              description: 'Proper description',
+              statusId: 1,
+              typeId: 2,
+            })
+            .expect(422),
+          // Invalid description type
+          supertest(app)
+            .post('/financial-entries')
+            .set('Authorization', token)
+            .send({
+              amount: 1000,
+              description: 1,
+              statusId: 1,
+              typeId: 2,
+            })
+            .expect(422),
+          // Invalid statusId type
+          supertest(app)
+            .post('/financial-entries')
+            .set('Authorization', token)
+            .send({
+              amount: 1000,
+              description: 'Proper description',
+              statusId: 'string',
+              typeId: 2,
+            })
+            .expect(422),
+          // Inexistent statusId
+          supertest(app)
+            .post('/financial-entries')
+            .set('Authorization', token)
+            .send({
+              amount: 1000,
+              description: 'Proper description',
+              statusId: 99,
+              typeId: 2,
+            })
+            .expect(422),
+          // Invalid typeId type
+          supertest(app)
+            .post('/financial-entries')
+            .set('Authorization', token)
+            .send({
+              amount: 1000,
+              description: 'Proper description',
+              statusId: 1,
+              typeId: 'string',
+            })
+            .expect(422),
+          // Inexistent typeId
+          supertest(app)
+            .post('/financial-entries')
+            .set('Authorization', token)
+            .send({
+              amount: 1000,
+              description: 'Proper description',
+              statusId: 1,
+              typeId: 99,
+            })
+            .expect(422),
+        ]);
+
+        responses.forEach((response) =>
+          expect(response.body.error).toBeDefined()
+        );
       });
     });
   });
