036eaadc56a9c48c80b621fdaa5bdf83b1f369d0
Renato Bispo <renatosilvabispo@outlook.com>
2022-04-22 13:56:22 -0300

    Write test for invalid email and missing information

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -4,11 +4,8 @@ import UserSeeder from '../prisma/seeders/user';
 import app from '../src/app';
 
 describe('POST /login', () => {
-
-  describe('If the request body contains email and password', () => {
-
-    describe('If the user exists and the password is valid', () => {
-
+  describe('If the request body contains valid email and password', () => {
+    describe('If the user exists and the password is correct', () => {
       it('Should respond with a token and status code 200', async () => {
         await new UserSeeder(prisma).seed();
 
@@ -21,10 +18,9 @@ describe('POST /login', () => {
       });
     });
 
-    describe('If the user exists and the password is invalid', () => {
-
+    describe('If the user exists and the password is wrong', () => {
       it('Should respond with an error and status code 401', async () => {
-        const expectedErrorMessage = 'Invalid email or password.';
+        const expectedErrorMessage = 'Wrong email or password.';
 
         await new UserSeeder(prisma).seed();
 
@@ -40,7 +36,7 @@ describe('POST /login', () => {
 
     describe("If the user doesn't exist", () => {
       it('Should respond with an error and status code 401', async () => {
-        const expectedErrorMessage = 'Invalid email or password.';
+        const expectedErrorMessage = 'Wrong email or password.';
 
         await new UserSeeder(prisma).seed();
 
@@ -52,6 +48,39 @@ describe('POST /login', () => {
         expect(response.body.error).toBeDefined();
         expect(response.body.error).toBe(expectedErrorMessage);
       });
+    });
+  });
+
+  describe('If the request body contains invalid email', () => {
+    it('Should respond with an error and status code 422', async () => {
+      const responses = await Promise.all([
+        supertest(app).post('/login').send({ email: '@email.com' }).expect(422),
+        supertest(app).post('/login').send({ email: '@email' }).expect(422),
+        supertest(app).post('/login').send({ email: 'email' }).expect(422),
+      ]);
+
+      responses.forEach((response) =>
+        expect(response.body.error).toBeDefined()
+      );
+    });
+  });
+
+  describe.only('If the request body has missing information', () => {
+    it('Should respond with an error and status code 422', async () => {
+      const responses = await Promise.all([
+        supertest(app)
+          .post('/login')
+          .send({ email: 'valid@email.com' })
+          .expect(422),
+        supertest(app)
+          .post('/login')
+          .send({ password: 'password' })
+          .expect(422),
+      ]);
+
+      responses.forEach((response) =>
+        expect(response.body.error).toBeDefined()
+      );
     });
   });
 });
