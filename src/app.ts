8f41abbc9e0d10e77bbeb72fa60ab7a081a0028a
Renato Bispo <renatosilvabispo@outlook.com>
2022-04-22 11:34:08 -0300

    Clear up code

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import helmet from 'helmet';
-import prisma from '../prisma';
 import { ErrorMiddleware } from './middlewares';
 import { LoginRoute } from './routes';
 
@@ -13,13 +12,6 @@ app.use(helmet());
 app.get('/ping', (_req, res) => res.status(200).json({ message: 'pong' }));
 
 app.use('/login', new LoginRoute().router);
-
-// Temporary endpoint definition to make sure tests are working
-app.get('/types', async (_req, res) => {
-  const types = await prisma.type.findMany({ select: { name: true } });
-
-  res.status(200).json(types.map(({ name }) => name));
-});
 
 app.use(ErrorMiddleware.handleError);
 
