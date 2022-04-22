0506bdc8a19386e50063c7c0e928d6d30feeec1a
Renato Bispo <renatosilvabispo@outlook.com>
2022-04-22 10:36:03 -0300

    Write test for happy case

---
+++ tests/login.test.ts
@@ -0,0 +1,21 @@
+import supertest from 'supertest';
+import prisma from '../prisma/index';
+import UserSeeder from '../prisma/seeders/user';
+import app from '../src/app';
+
+describe('POST /login', () => {
+  describe('If the request body contains email and password', () => {
+    describe('If the user exists', () => {
+      it('Should respond with a token', async () => {
+        await new UserSeeder(prisma).seed();
+
+        const response = await supertest(app)
+          .post('/login')
+          .send({ email: 'tony@soprano.com', password: 'theboss' })
+          .expect(201);
+
+        expect(response.body.token).toBeDefined();
+      });
+    });
+  });
+});
