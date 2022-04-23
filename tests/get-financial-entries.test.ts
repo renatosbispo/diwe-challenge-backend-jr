d069b08331cadd81f5856fc2bc1813f355fa7754
Renato Bispo <renatosilvabispo@outlook.com>
2022-04-22 21:53:39 -0300

    Write test for happy case

---
+++ tests/get-financial-entries.test.ts
@@ -0,0 +1,59 @@
+import { FinancialEntry } from '@prisma/client';
+import supertest from 'supertest';
+import prisma from '../prisma/index';
+import FinancialEntrySeeder from '../prisma/seeders/financial-entry';
+import StatusSeeder from '../prisma/seeders/status';
+import TypeSeeder from '../prisma/seeders/type';
+import UserSeeder from '../prisma/seeders/user';
+import app from '../src/app';
+
+describe('GET /financial-entries', () => {
+  const statusSeeder = new StatusSeeder(prisma);
+  const typeSeeder = new TypeSeeder(prisma);
+  const userSeeder = new UserSeeder(prisma);
+  const financialEntrySeeder = new FinancialEntrySeeder(prisma);
+
+  describe('If token is missing, invalid or expired', () => {
+    it('Should return an error and status code 401', async () => {
+      await supertest(app).get('/financial-entries').expect(401);
+    });
+  });
+
+  describe('If token is valid', () => {
+    let token: string;
+
+    beforeEach(async () => {
+      await statusSeeder.seed();
+      await typeSeeder.seed();
+      await userSeeder.seed();
+      await financialEntrySeeder.seed();
+
+      const response = await supertest(app)
+        .post('/login')
+        .send({ email: 'tony@soprano.com', password: 'theboss' });
+
+      token = response.body.token;
+
+      await supertest(app)
+        .post('/financial-entries')
+        .set('Authorization', token)
+        .send({
+          amount: 200000,
+          description: 'Golden ring for Carmela',
+          statusId: 1,
+          typeId: 1,
+        });
+    });
+
+    it('Should return only the financial entries of the user with status code 200', async () => {
+      const response = await supertest(app)
+        .get('/financial-entries')
+        .set('Authorization', token)
+        .expect(200);
+
+      response.body.forEach((financialEntry: FinancialEntry) =>
+        expect(financialEntry.userId === 1)
+      );
+    });
+  });
+});
